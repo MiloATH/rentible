6ca6197a188dccba96e8a6c15275050657c2c52c
Daniel Lee <dan.dongsung@gmail.com>
2017-04-08 19:28:55 -0400

    update stashed code

--- views/index.jsx
+++ views/index.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var Layout = require('./layout');
 var ItemGallery = require ('./itemGallery');
 var CategoryMenu = require ('./categoryMenu');
 var SignOn = require('./signon');
@@ -7,26 +8,17 @@ class Index extends React.Component {
   render() {
     return (
       <div>
-        <head>
-          <title>{this.props.title}</title>
-          <link rel="stylesheet" href="/stylesheets/header.css" />
-          <link rel="stylesheet" href="/stylesheets/homepage.css" />
-          <script dangerouslySetInnerHTML={{__html:`
-            // This is making use of ES6 template strings, which allow for
-            // multiline strings. We specified "{jsx: {harmony: true}}" when
-            // creating the engine in app.js to get this feature.
-            console.log("hello world");
-          `}}/>
-        </head>
-        <h1>
-          {this.props.title}
-        </h1>
-        <p>Welcome to {this.props.title}</p>
-        <SignOn />
-        <CategoryMenu />
-        <div id="bottomColor" />
-        <div id="bottomCover" />
-        <ItemGallery />
+        <Layout title={this.props.title}>
+          <h1>
+            {this.props.title}
+          </h1>
+          <p>Welcome to {this.props.title}</p>
+          <SignOn />
+          <CategoryMenu />
+          <div id="bottomColor" />
+          <div id="bottomCover" />
+          <ItemGallery />
+        </Layout>
       </div>
     )
   }
