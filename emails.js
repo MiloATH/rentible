e34d2e4ec016697371e68cd6acb79d054008471b
Milo Hartsoe <milo.red.duck@gmail.com>
2017-04-09 01:50:58 -0400

    Add emails to purchasers

--- emails.js
+++ emails.js
@@ -1,7 +1,7 @@
 var request = require('superagent');
 var sparkpostAPIURL = 'https://api.sparkpost.com/api/v1/transmissions';
 
-exports.sendEmail = function(to, subject, text) {
+function sendEmail(to, subject, text) {
     request.post(sparkpostAPIURL)
         .set('Content-Type', 'application/json')
         .set('Authorization', 'c063c1944d5c83d1fdf0186db38a97b81e3ca5fa')
@@ -29,4 +29,11 @@ exports.purchaseMadeEmail = function(buyer, seller, post, paymentResults, times)
   console.log("Seller: " + seller);
   console.log("Post: " + post);
   console.log("Payment results: " + paymentResults);
+  //Purchaser
+  var subject = "Rental Receipt"
+  var text = "Hey, thank you for shopping with rentible. Here is the receipt of your recent purchase. You rented " +
+  post.title + " for " + times + " " + post.perTime +". The total price was $" + paymentResults.body.objectCreated.amount +
+  ". You can contact the owner at " + seller.email + ".";
+
+  sendEmail(buyer.email, subject, text);  
 }
