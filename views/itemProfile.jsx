6da1f6bd33b880b4453fb305994e46dad84c164d
Daniel Lee <dan.dongsung@gmail.com>
2017-04-09 09:17:06 -0400

    final polishes

--- views/itemProfile.jsx
+++ views/itemProfile.jsx
@@ -5,27 +5,27 @@ class ItemProfile extends React.Component {
     return (
       <div>
         <div id="bottom">
-          <div id="itemImg">
-            <img title={this.props.datum.title} src={this.props.datum.image_url} />
-          </div>
           <div id="info">
             <div>
               <h1>{this.props.datum.title}</h1>
-              <h2>{this.props.datum.ownerId}</h2>
               <div id="maindescr">{this.props.datum.description}</div>
             </div>
-            <div id="buyholder">
+            <div id="buyholder" style={{textAlign: 'center'}}>
               <div id="buy">
-                <div id="price">
-                  {this.props.datum.price}
+                <div id="price" >
+                  ${this.props.datum.price} /{this.props.datum.perTime}
                 </div>
-                <div>
-                  {this.props.datum.perTime}
-                </div>
+                <br />
                 <button id="buyBtn" onclick="(function(){document.getElementById('meme').style.display = 'block';})()">Rent</button>
               </div>
             </div>
           </div>
+          <br />
+          <br />
+
+            <img title={this.props.datum.title} src={this.props.datum.image_url} style={{display: "block", margin: "auto", width:"70%"}} />
+
+          
         </div>
         <div id="meme">
           <form action="/api/offer">
